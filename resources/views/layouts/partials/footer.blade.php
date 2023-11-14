97d60ae74f4a917dc311b36b8f5c35d6aa647b1b
sanjaydharsans <sanjaydharsans@gmail.com>
2023-11-14 10:58:54 +0530

    first commit

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      Developed By San
+    </div>
+    <strong>&copy; <?php echo date('Y')?> - Point of Sale System</strong> 
+  </footer>
